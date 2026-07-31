7a35b8dada6e43951f7de239bfae80bfd3b2b352
BharathKumarReddy <nbkumar2103@gmail.com>
2026-07-31 12:45:59 +0530

    updated file

---
+++ 03-data-types/01-basic-data-types.py
@@ -0,0 +1,14 @@
+name = "Bharath"
+age = 36
+experience = 8.6
+is_devops_engineer = True
+
+print(name)
+print(age)
+print(experience)
+print(is_devops_engineer)
+
+print(type(name))
+print(type(age))
+print(type(experience))
+print(type(is_devops_engineer))
