e39c32c6aa56916240d763620b34a450b1288742
BharathKumarReddy <nbkumar2103@gmail.com>
2026-07-31 14:02:35 +0530

    updated file

---
+++ 04-input-output/03-profile.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ")
+company = input("Enter your company: ")
+role = input("Enter your role: ")
+
+print("\n----- Profile -----")
+print("Name    :", name)
+print("Company :", company)
+print("Role    :", role)
