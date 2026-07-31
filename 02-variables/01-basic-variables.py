21abbe55e2e724ae175bd7ff85daaf7ad4b12f41
BharathKumarReddy <nbkumar2103@gmail.com>
2026-07-31 12:40:38 +0530

    updated file

---
+++ 02-variables/01-basic-variables.py
@@ -0,0 +1,11 @@
+name = "Bharath"
+company = "ABC Technologies"
+role = "Senior DevSecOps Engineer"
+experience = 8.6
+learning = "Python"
+
+print(name)
+print(company)
+print(role)
+print(experience)
+print(learning)
---
+++ 02-variables/02-update-variable.py
@@ -0,0 +1,7 @@
+name = "Bharath"
+
+print(name)
+
+name = "Rahul"
+
+print(name)
