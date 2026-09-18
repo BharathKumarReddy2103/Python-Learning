e6ae0a3a2039f7d3ce5952b1188cd7f3dd548700
BharathKumarReddy <nbkumar2103@gmail.com>
2026-09-18 07:37:03 +0530

    updated file

---
+++ basics/a.py
@@ -0,0 +1,8 @@
+import b
+x = 5
+
+
+if __name__ == "__main__":
+    print("x")
+print(x)
+print(b.y)
---
+++ basics/b.py
@@ -0,0 +1,5 @@
+y = 10
+
+# entry point for execution
+if __name__ == "__main__":
+    print(y)
