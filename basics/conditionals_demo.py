f5dbee4dda9a2ef3d1869ac39343878bee6a7455
BharathKumarReddy <nbkumar2103@gmail.com>
2026-09-22 16:38:52 +0530

    updated file

---
+++ basics/conditionals_demo.py
@@ -0,0 +1,9 @@
+x = 20
+
+if x > 4:
+    x = x + 1
+    print(x)
+else:
+    x = x - 1
+    print(x)
+print("completed")
---
+++ basics/test_program.py
@@ -0,0 +1,7 @@
+x = -5
+if x > 2:
+    x = x + 1
+     print(x)
+else:
+    x = x - 1
+    print(x)
